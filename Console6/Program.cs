c707c3ea33d296e6a5fa10148791087186cd2c4b
nazzz328 <nazzz328@gmail.com>
2021-08-18 12:30:57 +0200

    First commit

---
+++ Console6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Console6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Drinks;
+    {
+        public string type;
+        public string color;
+        public int volume;
+        public bool bottled;
+    }
+}
